6b4c074f4dd8c1d115edab9aa786a73ff27cc542
Ali Haskara <alihaskara@gmail.com>
2016-06-16 17:51:02 +0300

    veritabanı class

---
+++ binaTakip/Database/daire.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace binaTakip.Database
+{
+    class daire
+    {
+        public int daireId { get; set; }
+        public int binaId { get; set; }
+        public int kisiId { get; set; }
+        public double odenen { get; set; }
+        public double kalan  { get; set; }
+        public double toplam { get; set; }
+       
+    }
+}
---
+++ binaTakip/Database/kisiler.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace binaTakip.Database
+{
+    class kisiler
+    {
+        public int binaId { get; set; }
+        public int kisiId { get; set; }
+        public string kisiAdi { get; set; }
+        public string daireId { get; set; }
+        public string telefon { get; set; }
+        public string durum { get; set; }
+        
+    }
+}
